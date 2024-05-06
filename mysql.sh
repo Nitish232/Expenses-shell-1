6654b5079eaf6ce1eba5ca72c85376b83d757e7b
Nitish232 <samanthakurthinitish@gmail.com>
2024-05-06 11:25:12 +0530

    1

--- mysql.sh
+++ mysql.sh
@@ -4,21 +4,20 @@ source ./common.sh
 
 check_root
 
-echo "Please enter DB passsword : "
+echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
 
 systemctl enable mysqld &>>$LOGFILE
+
 systemctl start mysqld &>>$LOGFILE
 
-#below code will be usefull for idempotent nature
-
-mysql -h 18.212.255.80 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+#Below code will be useful for idempotent nature
+mysql -h 172.31.24.133 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
-then 
+then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 else
-    echo -e "Mysql root password is already setup...$Y Skipping $N"
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
-
