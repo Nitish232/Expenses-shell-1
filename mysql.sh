61fd8d24103293fd2de9824cf67dc623cb9c948d
Nitish232 <samanthakurthinitish@gmail.com>
2024-05-06 12:34:49 +0530

    1

--- mysql.sh
+++ mysql.sh
@@ -14,7 +14,7 @@ systemctl enable mysqld &>>$LOGFILE
 systemctl start mysqld &>>$LOGFILE
 
 #Below code will be useful for idempotent nature
-mysql -h 172.31.28.10 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.28.10 -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
