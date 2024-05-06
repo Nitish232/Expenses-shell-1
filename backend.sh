e1b65edcb4ece1be1288cb5bd32fbc560fd54c8c
Nitish232 <samanthakurthinitish@gmail.com>
2024-05-06 12:39:45 +0530

    1

--- backend.sh
+++ backend.sh
@@ -29,7 +29,7 @@ unzip /tmp/backend.zip &>>$LOGFILE
 
 npm install &>>$LOGFILE
 
-cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/Expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
 systemctl start backend &>>$LOGFILE
